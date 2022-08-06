93e3afd1890d769ff56404c8e1753196408efeee
luiliomota <luiliosm@yahoo.com.br>
2022-08-06 08:31:22 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 const form = document.getElementById("novoItem")
 
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
- 
-    const item = evento.target.elements['nome'].value;
-    const quantidade = evento.target.elements['quantidade'].value;
-
-    console.log(item, quantidade)
+    evento.preventDefault();
+    console.log(evento);
 })
-
-// forma simples - console.log(evento.target[0].value)
-// forma dinâmica - console.log(evento.target.elements['nome'].value)
